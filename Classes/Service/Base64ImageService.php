78273285007d8502eaec13491a10c252970bad2e
Maik Schneider <m.schneider@blueways.de>
2018-01-25 04:09:56 +0100

    ADD Base64ImageService for inline svg generation

---
+++ Classes/Service/Base64ImageService.php
@@ -0,0 +1,41 @@
+<?php
+namespace Blueways\BwPlaceholderImages\Service;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Core\SingletonInterface;
+
+/**
+ * Class GifBuilderService
+ * @package Blueways\BwPlaceholderImages\Service
+ */
+class Base64ImageService implements SingletonInterface
+{
+    public function generateSvg($color, $width = 1, $height = 1)
+    {
+        $colors = json_decode($color);
+
+        if(!count($colors)) return '';
+
+        for($i=0;$i<count($colors);$i++){
+            for($j=0;$j<count($colors[$i]);$j++){
+                $colors[$i][$j] = substr($colors[$i][$j], 1);
+            }
+        }
+        $height05 = ceil($height / 2);
+
+        $svg = "data:image/svg+xml,%3csvg viewBox='0 0 ".$width." ".$height."' xmlns='http://www.w3.org/2000/svg'%3e%3cdefs%3e%3clinearGradient id='b'%3e%3cstop offset='0' stop-color='%23".$colors[0][0]."'/%3e%3cstop offset='50%25' stop-color='%23".$colors[1][0]."'/%3e%3cstop offset='100%25' stop-color='%23".$colors[2][0]."'/%3e%3c/linearGradient%3e%3clinearGradient id='c'%3e%3cstop offset='0' stop-color='%23".$colors[2][0]."'/%3e%3cstop offset='50%25' stop-color='%23".$colors[2][1]."'/%3e%3cstop offset='100%25' stop-color='%23".$colors[2][2]."'/%3e%3c/linearGradient%3e%3clinearGradient id='d' x2='0' y2='1'%3e%3cstop offset='0' stop-color='%23".$colors[1][0]."'/%3e%3cstop offset='50%25' stop-color='%23".$colors[1][1]."'/%3e%3cstop offset='100%25' stop-color='%23".$colors[1][2]."'/%3e%3c/linearGradient%3e%3clinearGradient id='a' x2='0' y2='1'%3e%3cstop offset='0' stop-color='white' stop-opacity='0'/%3e%3cstop offset='50%25' stop-color='white'/%3e%3cstop offset='100%25' stop-color='white' stop-opacity='0'/%3e%3c/linearGradient%3e%3cmask id='e'%3e%3cpath fill='url(%23a)' d='M0 0h".$width."v".$height."H0z'/%3e%3c/mask%3e%3c/defs%3e%3cpath fill='url(%23b)' d='M0 0h".$width."v".$height05."H0z'/%3e%3cpath fill='url(%23c)' d='M0 ".$height05."h".$width."v".$height05."H0z'/%3e%3cpath fill='url(%23d)' mask='url(%23e)' d='M0 0h".$width."v".$height."H0z'/%3e%3c/svg%3e";
+
+        return $svg;
+    }
+}
---
+++ Classes/ViewHelpers/LazyPlaceholderViewhelper.php
@@ -0,0 +1,73 @@
+<?php
+namespace Blueways\BwPlaceholderImages\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\ViewHelpers\Uri\ImageViewHelper;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use Blueways\BwPlaceholderImages\Service\Base64ImageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class LazyPlaceholderViewhelper extends ImageViewHelper implements \TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperInterface
+{
+
+    public function initilizeArguments()
+    {
+        parent::initilizeArguments();
+
+        $this->registerArgument('fallbackBgColor', 'string', 'Fallback color to use when image has no dominant color', false, '#EEEEEE');
+    }
+
+    /**
+     * Resizes the image (if required) and returns its path. If the image was not resized, the path will be equal to $src
+     *
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     * @throws Exception
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $src = $arguments['src'];
+        $image = $arguments['image'];
+        $treatIdAsReference = $arguments['treatIdAsReference'];
+
+        if ((is_null($src) && is_null($image)) || (!is_null($src) && !is_null($image))) {
+            throw new Exception('You must either specify a string src or a File object.', 1460976233);
+        }
+
+        try {
+            $imageService = self::getImageService();
+            $image = $imageService->getImage($src, $image, $treatIdAsReference);
+
+            $dominantColors = $image->getProperty('dominant_colors');
+
+            $base64ImageService = self::getBase64ImageService();
+            $svg = $base64ImageService->generateSvg($dominantColors, $arguments['width'], $arguments['height']);
+
+            return $svg;
+        } catch (ResourceDoesNotExistException $e) {
+            // thrown if file does not exist
+        } catch (\UnexpectedValueException $e) {
+            // thrown if a file has been replaced with a folder
+        } catch (\RuntimeException $e) {
+            // RuntimeException thrown if a file is outside of a storage
+        } catch (\InvalidArgumentException $e) {
+            // thrown if file storage does not exist
+        }
+        return '';
+    }
+
+    /**
+     * Return an instance of Base64ImageService using object manager
+     *
+     * @return Base64ImageService
+     */
+    protected static function getBase64ImageService()
+    {
+        /** @var ObjectManager $objectManager */
+        $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+        return $objectManager->get(Base64ImageService::class);
+    }
+
+}
